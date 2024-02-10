824d2a652efd144269219f7020f215e6cca5c6d1
ucf26 <117460543+ucf26@users.noreply.github.com>
2024-02-10 10:20:26 +0200

    Update mcal_external_interrup.h

--- MCAL_Layer/Interrupt/mcal_external_interrup.h
+++ MCAL_Layer/Interrupt/mcal_external_interrup.h
@@ -14,7 +14,40 @@
 /* Section : Macro Declarations */
 
 /* Section : Macro Functions Declarations */
+/* This macro will enable external interrupt INT0 */
+#define EXT_INT0_InterruptEnable() (INTCONbits.INT0E = 1)
+/* This macro will disable external interrupt INT0 */
+#define EXT_INT0_InterruptEnable() (INTCONbits.INT0E = 0)
+/* This macro will clear the flag bit for external interrupt INT0 */
+#define EXT_EXT_INT0_InterruptFlagClear (INTCONbits.INT0IE = 0)
+/* This macro will set INT0 trigger as Rising Edge */
+#define EXT_EXT_INT0_RisingEdgeSet() (INTCON2bits.INTEDG0 = 1)
+/* This macro will set INT0 trigger as Falling Edge */
+#define EXT_EXT_INT0_FallingEdgeSet() (INTCON2bits.INTEDG0 = 0)
 
+
+/* This macro will enable external interrupt INT1 */
+#define EXT_INT1_InterruptEnable() (INTCON3bits.INT1E = 1)
+/* This macro will disable external interrupt INT1 */
+#define EXT_INT1_InterruptEnable() (INTCON3bits.INT1E = 0)
+/* This macro will clear the flag bit for external interrupt INT1 */
+#define EXT_EXT_INT1_InterruptFlagClear (INTCON3bits.INT1F = 0)
+/* This macro will set INT1 trigger as Rising Edge */
+#define EXT_EXT_INT1_RisingEdgeSet() (INTCON2bits.INTEDG1 = 1)
+/* This macro will set INT1 trigger as Falling Edge */
+#define EXT_EXT_INT1_FallingEdgeSet() (INTCON2bits.INTEDG1 = 0)
+
+
+/* This macro will enable external interrupt INT2 */
+#define EXT_INT2_InterruptEnable() (INTCON3bits.INT2E = 1)
+/* This macro will disable external interrupt INT2 */
+#define EXT_INT2_InterruptEnable() (INTCON3bits.INT2E = 0)
+/* This macro will clear the flag bit for external interrupt INT2 */
+#define EXT_EXT_INT2_InterruptFlagClear (INTCON3bits.INT2F = 0)
+/* This macro will set INT2 trigger as Rising Edge */
+#define EXT_EXT_INT2_RisingEdgeSet() (INTCON2bits.INTEDG2 = 1)
+/* This macro will set INT2 trigger as Falling Edge */
+#define EXT_EXT_INT2_FallingEdgeSet() (INTCON2bits.INTEDG2 = 0)
 
 /* Section : Data Type Declarations */
 
@@ -25,4 +58,3 @@
 
 
 #endif	/* MCAL_EXTERNAL_INTERRUP_H */
-
