d5e05361299fca1939f418ef6c30c0d52a4d4bf7
yousef <yousefzahra382@gmail.com>
2024-07-04 15:25:11 +0300

    Update appplication.c

--- appplication.c
+++ appplication.c
@@ -11,6 +11,18 @@
 i2c_t i2c_obj;
 uint8 rep;
 
+#define SLAVE_1 0x60
+#define SLAVE_2 0x62
+
+void MSSP_I2C_Send_1_Byte(uint8 add, uint8 data){
+    Std_ReturnType ret = E_NOT_OK;
+    ret = MSSP_I2C_Master_Send_Start(&i2c_obj);
+    ret = MSSP_I2C_Master_Write_Blocking(&i2c_obj, add, &rep);
+    ret = MSSP_I2C_Master_Write_Blocking(&i2c_obj, data, &rep);
+    ret = MSSP_I2C_Master_Send_Stop(&i2c_obj);
+}
+
+
 int main() {
     Std_ReturnType ret = E_NOT_OK;
     
@@ -22,19 +34,19 @@ int main() {
     i2c_obj.I2C_DefaultInterruptHandler = NULL;
     
     ret = MSSP_I2C_Init(&i2c_obj);
-    ret = MSSP_I2C_Master_Send_Start(&i2c_obj);
     
     while(1)
     {
-        ret = MSSP_I2C_Master_Send_Start(&i2c_obj);
+        MSSP_I2C_Send_1_Byte(SLAVE_1, 0x55);
         __delay_ms(1000);
-        // ret = MSSP_I2C_Master_Send_Repeated_Start(&i2c_obj);
-        ret = MSSP_I2C_Master_Write_Blocking(&i2c_obj, 'Y', &rep);
-        ret = MSSP_I2C_Master_Write_Blocking(&i2c_obj, 'o', &rep);
-        ret = MSSP_I2C_Master_Write_Blocking(&i2c_obj, 'u', &rep);
-
+        MSSP_I2C_Send_1_Byte(SLAVE_2, 0x55);
         __delay_ms(1000);
-        ret = MSSP_I2C_Master_Send_Stop(&i2c_obj);
+        MSSP_I2C_Send_1_Byte(SLAVE_1, 0x55);
+        __delay_ms(1000);
+        MSSP_I2C_Send_1_Byte(SLAVE_2, 0x55);
+        __delay_ms(1000);
+        
+        
     }
     return (EXIT_SUCCESS);
 }
